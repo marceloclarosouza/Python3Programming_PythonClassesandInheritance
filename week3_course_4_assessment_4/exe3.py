0e6dbe7c81b033b05b6edcaf836fbe2d7db2c213
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-07-07 15:13:08 -0300

    Create exe3.py

---
+++ week3_course_4_assessment_4/exe3.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  7 15:04:12 2020
+
+@author: mcsbi
+"""
+"""
+The list, numb, contains integers. Write code that populates the list remainder with the remainder of 36 divided by each number in numb. For example, the first element should be 0, because 36/6 has no remainder. If there is an error, have the string “Error” appear in the remainder.
+"""
+numb = [6, 0, 36, 8, 2, 36, 0, 12, 60, 0, 45, 0, 3, 23]
+
+remainder = []
+for n in numb:
+    try:
+        x = 36%n
+        remainder.append(x)
+    except:
+        remainder.append("Error")
+print(remainder)
