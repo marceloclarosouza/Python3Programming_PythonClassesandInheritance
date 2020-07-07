4aeff2fdc50e8ddd6ce501c0a4611b49b3189451
MarceloClaroSouza <marcelo.claro.souza@gmail.com>
2020-07-07 15:56:58 -0300

    Create exe4.py

---
+++ week3_course_4_assessment_4/exe4.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  7 15:31:32 2020
+
+@author: mcsbi
+"""
+
+"""
+Provided is buggy code, insert a try/except so that the code passes.
+"""
+
+lst = [2,4,10,42,12,0,4,7,21,4,83,8,5,6,8,234,5,6,523,42,34,0,234,1,435,465,56,7,3,43,23]
+
+lst_three = []
+
+for num in lst:
+    try: 
+        num != 0
+        if 3 % num == 0:
+            lst_three.append(num)
+    except Exception:
+        pass
+
+print(lst_three)
